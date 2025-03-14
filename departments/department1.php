71c8adf07d7d7c7f9ff5e7d7dfe2f6fcc18485c7
RobinGroeneveld <robin@logicandmore.com>
2025-03-14 12:02:30 +0100

    Enhance styling and layout in department1.php and main.css for improved user experience

--- departments/department1.php
+++ departments/department1.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>taakenlijst</title>
+    <link rel="stylesheet" href="../public_html/css/main.css">
+    <title>takenlijst</title>
 </head>
 <body>
     <main>
