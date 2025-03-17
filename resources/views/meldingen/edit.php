65cb4f66e2afd17df59d3228968c5d624bb15aa6
ArtemisStorm20 <D309906@edu.curio.nl>
2025-03-17 12:06:59 +0100

    delete gemaakt

--- resources/views/meldingen/edit.php
+++ resources/views/meldingen/edit.php
@@ -9,14 +9,22 @@
 
 <body>
 
-
     <?php require_once __DIR__.'/../components/header.php'; ?>
 
     <div class="container">
-        <h1>taak aanpassen</h1>
+        <h1>Taak aanpassen</h1>
 
         <form action="<?php echo $base_url; ?>/app/Http/Controllers/meldingenController.php" method="POST">
             <input type="submit" value="Verstuur melding">
+        </form>
+
+        <!-- Delete Confirmation Form -->
+        <h2>Verwijder Record</h2>
+        <form action="edit.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+            <p>Weet je zeker dat je dit record wilt verwijderen?</p>
+            <button type="submit" name="action" value="delete">Verwijder</button>
+            <a href="index.php">Annuleer</a>
         </form>
     </div>
 
