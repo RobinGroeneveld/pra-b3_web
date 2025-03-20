7b242682497776d06bcb3fef8e82e1ee6222c1a7
ArtemisStorm20 <D309906@edu.curio.nl>
2025-03-20 13:50:23 +0100

    afdeling.php verwijders en op goede plek gezet

--- departments/personeel.php
+++ departments/personeel.php
@@ -1,3 +1,15 @@
+<?php
+require_once __DIR__.'/../../../config/conn.php';
+
+$afdeling = $_GET['afdeling'] ?? '';
+
+$query = "SELECT title, afdeling FROM taken WHERE afdeling = :afdeling";
+$statement = $conn->prepare($query);
+$statement->bindParam(':afdeling', $afdeling);
+$statement->execute();
+$tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
