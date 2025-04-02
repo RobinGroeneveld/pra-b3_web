512947bab4f4fb3ff6aa9c9e4d8e25f3627d87c9
RobinGroeneveld <robin@logicandmore.com>
2025-04-02 14:23:51 +0200

    Refactor login and task overview pages with improved header layout and CSS enhancements

--- backend/login.php
+++ backend/login.php
@@ -8,17 +8,28 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public_html/css/main.css">
     <title>inloggen</title>
 </head>
 <body>
     <header>
-
+        <div class="wrapper">
+            <div class="alignment">
+                <div class="home-icon">
+                    <a href="../../../index.php"><i class="fa-solid fa-house"></i></a>
+                </div>
+                <div class="header-tekst">
+                    <h3>Welkom bij het overzicht waar de taken niet klaar zijn</h3>
+                </div>
+            </div>
+        </div>
     </header>
     <main>
-    <form action="loginController.php" method="POST">
-        <input type="text" name="username" placeholder="user">
-        <input type="password" name="password" placeholder="pass">
-        <input type="submit" value="Inloggen">
+        <form action="loginController.php">
+            <input type="text" name="username" placeholder = "gebruikersnaam">
+            <input type="password" name="password" placeholder="wachtwoord">
+            <input type="submit" value="Inloggen">
+        </form>
     </main>
     
 </body>
